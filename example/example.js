319445d013ad87d25b18c34edb8e5765de7b84bf
王威威 <wangweiwei@qutoutiao.net>
2020-01-16 16:10:02 +0800

    refactor: 优化例子

--- example/example.js
+++ example/example.js
@@ -14,18 +14,28 @@ var app = new Vue({
   el: "#app",
   data() {
     return {
-      width: 0,
-      height: 0
+      width1: 0,
+      height1: 0,
+
+      width2: 0,
+      height2: 0
     };
   },
   mounted() {
-    this.width = this.$el.offsetWidth;
-    this.height = this.$el.offsetHeight;
+    this.width1 = this.$refs.resize1.offsetWidth;
+    this.height1 = this.$refs.resize1.offsetHeight;
+
+    this.width2 = this.$refs.resize2.offsetWidth;
+    this.height2 = this.$refs.resize2.offsetHeight;
   },
   methods: {
-    onResize({ width, height }) {
-      this.width = width;
-      this.height = height;
+    onResize1({ width, height }) {
+      this.width1 = width;
+      this.height1 = height;
+    },
+    onResize2({ width, height }) {
+      this.width2 = width;
+      this.height2 = height;
     }
   }
 });
